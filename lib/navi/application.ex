287f0d24d222949bc18963fa98c0bb7c6fa8ad67
Ryan Winchester <ryan@sevenshores.co>
2021-05-20 11:32:18 -0700

    Add a consumer

--- lib/navi/application.ex
+++ lib/navi/application.ex
@@ -8,8 +8,7 @@ defmodule Navi.Application do
   @impl true
   def start(_type, _args) do
     children = [
-      # Starts a worker by calling: Navi.Worker.start_link(arg)
-      # {Navi.Worker, arg}
+      Navi.TestConsumer
     ]
 
     # See https://hexdocs.pm/elixir/Supervisor.html
